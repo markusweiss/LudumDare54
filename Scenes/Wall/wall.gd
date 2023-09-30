102db989f0b5c0033548d33ae2183654993f6ecc
markusweiss <git@code71.de>
2023-09-30 14:36:08 +0200

    first shader test :)

--- Scenes/Wall/wall.gd
+++ Scenes/Wall/wall.gd
@@ -1,16 +1,16 @@
 extends Area2D
 
-var speed = 40  # Geschwindigkeit, mit der sich das Hindernis bewegt
-var target_x = 0  # X-Koordinate, auf die das Hindernis zusteuert
+var speed = 100  # Geschwindigkeit, mit der sich das Hindernis bewegt
+var target_y = 0  # X-Koordinate, auf die das Hindernis zusteuert
 var direction = Vector2.ZERO
 var magnet = false
 
 func _ready():
 	# Setzen Sie die anfängliche Richtung basierend auf der aktuellen Position
-	if position.x < target_x && !magnet:
-		direction = Vector2(1, 0)  # Bewegung nach rechts
+	if position.y < target_y && !magnet:
+		direction = Vector2(0, -1)  # Bewegung nach rechts
 	else:
-		direction = Vector2(-1, 0)  # Bewegung nach links
+		direction = Vector2(0, 1)  # Bewegung nach links
 
 func _physics_process(delta):
 	if magnet:
@@ -18,7 +18,7 @@ func _physics_process(delta):
 	else:
 		position += direction * speed * delta
 		
-	if position.x >= 500:
+	if position.y <= 10:
 		magnet=false
 	
 
